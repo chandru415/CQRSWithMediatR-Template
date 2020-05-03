be9650dc3cb619f5aaa9383f1ba1bc2bf21ca021
chandru415 <chandru9052@gmail.com>
2020-05-03 22:07:58 +0530

    [chandu] Application Auto map implementation

---
+++ CQRSWithMediatR-Template/Application/Common/Mappings/IMapFrom.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Common.Mappings
+{
+    public interface IMapFrom<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
