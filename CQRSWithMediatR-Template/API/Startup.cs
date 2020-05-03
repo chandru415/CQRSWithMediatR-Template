ae3dd1b303843861475eef4ffc41df0890c3cde6
chandru415 <chandru9052@gmail.com>
2020-05-03 22:19:50 +0530

    [chandu] API - Implemented swagger

--- CQRSWithMediatR-Template/API/Startup.cs
+++ CQRSWithMediatR-Template/API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -23,6 +24,11 @@ namespace API
             services.AddControllers();
             services.AddApplication();
             services.AddInfrastructure();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CQRS With MediatR", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +44,13 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CQRS With MediatR V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
