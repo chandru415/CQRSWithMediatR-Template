787b73c0e6d5147a3f7e00dc94023e9eb85a9262
chandru415 <chandru9052@gmail.com>
2020-05-03 13:38:38 +0530

    [chandu] Domain - structure & default classes
    Added Auditable Class & EntityBase & other folders

---
+++ CQRSWithMediatR-Template/Domain/Common/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Common
+{
+    public abstract class EntityBase: AuditableEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
