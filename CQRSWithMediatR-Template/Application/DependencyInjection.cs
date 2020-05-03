59d4f251a696415ce1354dae3d4e5bbb9c74a4f3
chandru415 <chandru9052@gmail.com>
2020-05-03 15:30:04 +0530

    [chandu] application - added MediatR & DependencyInjection

---
+++ CQRSWithMediatR-Template/Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+
+            return services;
+        }
+    }
+}
