d3b9e0cca8cf9bfc1cf3fdbe5d6367c26aa47366
chandru415 <chandru9052@gmail.com>
2020-05-11 23:49:02 +0530

    [chandu] Infrastructure - Implemented prerequiste for the db connection

---
+++ CQRSWithMediatR-Template/Application/Common/Interfaces/IDbContextService.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Application.Common.Interfaces
+{
+    public interface IDbContextService
+    {
+        /** He we can return connection string */
+        string DbContext();
+        /** He we can return type of connection
+         * this will follow the desire Db connection
+         * we wish to establish */
+        SqlConnection CreateDbConnection();
+    }
+}
---
+++ CQRSWithMediatR-Template/Infrastructure/Persistence/DbContextService.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.Interfaces;
+using System;
+using System.Data.SqlClient;
+
+namespace Infrastructure.Persistence
+{
+    internal class DbContextService : IDbContextService
+    {
+        public SqlConnection CreateDbConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DbContext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
